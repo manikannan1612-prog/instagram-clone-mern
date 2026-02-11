17fc7470a3098b1c752e751730965f8ddca14128
Your Name <you@example.com>
2026-02-11 12:30:08 +0530

    vercell

--- index.js
+++ index.js
@@ -226,13 +226,6 @@ app.listen(PORT,()=>{
 const path = require('path');
 
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 
 
 
