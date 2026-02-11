037309750e717d78839e4abe1a4a662ca72420b6
Your Name <you@example.com>
2026-02-11 11:56:17 +0530

    finaly

--- index.js
+++ index.js
@@ -223,5 +223,19 @@ app.listen(PORT,()=>{
 
 });
 
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+
+
+
 module.exports = app;
 
